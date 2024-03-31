4d9ef249a8086db5828d078e1912b9be977b6a2f
NadeemIqbal6614 <157383949+NadeemIqbal6614@users.noreply.github.com>
2024-03-31 06:04:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,179 @@
+const ayah = {
+    juz1: {
+      audio:
+        "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/1.mp3",
+      ayyahs: [
+        "بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ",
+        "ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَـٰلَمِينَ",
+        "ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ ٣",
+      ],
+    },
+    juz2: {
+      audio:
+        "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/2.mp3",
+      ayyahs: [
+        "سَيَقُولُ ٱلسُّفَهَآءُ مِنَ ٱلنَّاسِ مَا وَلَّىٰهُمْ عَن قِبْلَتِهِمُ ٱلَّتِى كَانُوا۟ عَلَيْهَا ۚ قُل لِّلَّهِ ٱلْمَشْرِقُ وَٱلْمَغْرِبُ ۚ يَهْدِى مَن يَشَآءُ إِلَىٰ صِرَٰطٍۢ مُّسْتَقِيمٍۢ",
+      ],
+    },
+    juz3: {
+      audio:
+        "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/3.mp3",
+      ayyahs: [
+        "۞ تِلْكَ ٱلرُّسُلُ فَضَّلْنَا بَعْضَهُمْ عَلَىٰ بَعْضٍۢ ۘ مِّنْهُم مَّن كَلَّمَ ٱللَّهُ ۖ وَرَفَعَ بَعْضَهُمْ دَرَجَـٰتٍۢ ۚ وَءَاتَيْنَا عِيسَى ٱبْنَ مَرْيَمَ ٱلْبَيِّنَـٰتِ وَأَيَّدْنَـٰهُ بِرُوحِ ٱلْقُدُسِ ۗ وَلَوْ شَآءَ ٱللَّهُ مَا ٱقْتَتَلَ ٱلَّذِينَ مِنۢ بَعْدِهِم مِّنۢ بَعْدِ مَا جَآءَتْهُمُ ٱلْبَيِّنَـٰتُ وَلَـٰكِنِ ٱخْتَلَفُوا۟ فَمِنْهُم مَّنْ ءَامَنَ وَمِنْهُم مَّن كَفَرَ ۚ وَلَوْ شَآءَ ٱللَّهُ مَا ٱقْتَتَلُوا۟ وَلَـٰكِنَّ ٱللَّهَ يَفْعَلُ مَا يُرِيدُ ٢٥٣",
+      ],
+    },
+    juz4: {
+      audio:
+        "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/4.mp3",
+      ayyahs: [
+        "۞ كُلُّ ٱلطَّعَامِ كَانَ حِلًّۭا لِّبَنِىٓ إِسْرَٰٓءِيلَ إِلَّا مَا حَرَّمَ إِسْرَٰٓءِيلُ عَلَىٰ نَفْسِهِۦ مِن قَبْلِ أَن تُنَزَّلَ ٱلتَّوْرَىٰةُ ۗ قُلْ فَأْتُوا۟ بِٱلتَّوْرَىٰةِ فَٱتْلُوهَآ إِن كُنتُمْ صَـٰدِقِينَ ٩٣",
+      ],
+    },
+    juz5: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/5.mp3",
+        ayyahs: [
+          "۞ وَٱلْمُحْصَنَـٰتُ مِنَ ٱلنِّسَآءِ إِلَّا مَا مَلَكَتْ أَيْمَـٰنُكُمْ ۖ كِتَـٰبَ ٱللَّهِ عَلَيْكُمْ ۚ وَأُحِلَّ لَكُم مَّا وَرَآءَ ذَٰلِكُمْ أَن تَبْتَغُوا۟ بِأَمْوَٰلِكُم مُّحْصِنِينَ غَيْرَ مُسَـٰفِحِينَ ۚ فَمَا ٱسْتَمْتَعْتُم بِهِۦ مِنْهُنَّ فَـَٔاتُوهُنَّ أُجُورَهُنَّ فَرِيضَةًۭ ۚ وَلَا جُنَاحَ عَلَيْكُمْ فِيمَا تَرَٰضَيْتُم بِهِۦ مِنۢ بَعْدِ ٱلْفَرِيضَةِ ۚ إِنَّ ٱللَّهَ كَانَ عَلِيمًا حَكِيمًۭا ٢٤",
+        ],
+      },
+      juz6: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/6.mp3",
+        ayyahs: [
+          "۞ لَّا يُحِبُّ ٱللَّهُ ٱلْجَهْرَ بِٱلسُّوٓءِ مِنَ ٱلْقَوْلِ إِلَّا مَن ظُلِمَ ۚ وَكَانَ ٱللَّهُ سَمِيعًا عَلِيمًا ١٤٨",
+        ],
+      },
+      juz7: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/7.mp3",
+        ayyahs: [
+          "۞ لَتَجِدَنَّ أَشَدَّ ٱلنَّاسِ عَدَٰوَةًۭ لِّلَّذِينَ ءَامَنُوا۟ ٱلْيَهُودَ وَٱلَّذِينَ أَشْرَكُوا۟ ۖ وَلَتَجِدَنَّ أَقْرَبَهُم مَّوَدَّةًۭ لِّلَّذِينَ ءَامَنُوا۟ ٱلَّذِينَ قَالُوٓا۟ إِنَّا نَصَـٰرَىٰ ۚ ذَٰلِكَ بِأَنَّ مِنْهُمْ قِسِّيسِينَ وَرُهْبَانًۭا وَأَنَّهُمْ لَا يَسْتَكْبِرُونَ ٨٢"
+        ],
+      },
+      juz8: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/8.mp3",
+        ayyahs: [
+        "وَلِكُلٍّۢ دَرَجَـٰتٌۭ مِّمَّا عَمِلُوا۟ ۚ وَمَا رَبُّكَ بِغَـٰفِلٍ عَمَّا يَعْمَلُونَ ١٣٢ وَرَبُّكَ ٱلْغَنِىُّ ذُو ٱلرَّحْمَةِ ۚ إِن يَشَأْ يُذْهِبْكُمْ وَيَسْتَخْلِفْ مِنۢ بَعْدِكُم مَّا يَشَآءُ كَمَآ أَنشَأَكُم مِّن ذُرِّيَّةِ قَوْمٍ ءَاخَرِينَ ١٣٣ إِنَّ مَا تُوعَدُونَ لَـَٔاتٍۢ ۖ وَمَآ أَنتُم بِمُعْجِزِينَ ١٣٤ قُلْ يَـٰقَوْمِ ٱعْمَلُوا۟ عَلَىٰ مَكَانَتِكُمْ إِنِّى عَامِلٌۭ ۖ فَسَوْفَ تَعْلَمُونَ مَن تَكُونُ لَهُۥ عَـٰقِبَةُ ٱلدَّارِ ۗ إِنَّهُۥ لَا يُفْلِحُ ٱلظَّـٰلِمُونَ ١٣٥ وَجَعَلُوا۟ لِلَّهِ مِمَّا ذَرَأَ مِنَ ٱلْحَرْثِ وَٱلْأَنْعَـٰمِ نَصِيبًۭا فَقَالُوا۟ هَـٰذَا لِلَّهِ بِزَعْمِهِمْ وَهَـٰذَا لِشُرَكَآئِنَا ۖ فَمَا كَانَ لِشُرَكَآئِهِمْ فَلَا يَصِلُ إِلَى ٱللَّهِ ۖ وَمَا كَانَ لِلَّهِ فَهُوَ يَصِلُ إِلَىٰ شُرَكَآئِهِمْ ۗ سَآءَ مَا يَحْكُمُونَ ١٣٦ وَكَذَٰلِكَ زَيَّنَ لِكَثِيرٍۢ مِّنَ ٱلْمُشْرِكِينَ قَتْلَ أَوْلَـٰدِهِمْ شُرَكَآؤُهُمْ لِيُرْدُوهُمْ وَلِيَلْبِسُوا۟ عَلَيْهِمْ دِينَهُمْ ۖ وَلَوْ شَآءَ ٱللَّهُ مَا فَعَلُوهُ ۖ فَذَرْهُمْ وَمَا يَفْتَرُونَ ١٣٧"
+        ],
+      },
+      juz9: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/9.mp3",
+        ayyahs: [
+          "وَلَوْ أَنَّ أَهْلَ ٱلْقُرَىٰٓ ءَامَنُوا۟ وَٱتَّقَوْا۟ لَفَتَحْنَا عَلَيْهِم بَرَكَـٰتٍۢ مِّنَ ٱلسَّمَآءِ وَٱلْأَرْضِ وَلَـٰكِن كَذَّبُوا۟ فَأَخَذْنَـٰهُم بِمَا كَانُوا۟ يَكْسِبُونَ ٩٦ أَفَأَمِنَ أَهْلُ ٱلْقُرَىٰٓ أَن يَأْتِيَهُم بَأْسُنَا بَيَـٰتًۭا وَهُمْ نَآئِمُونَ ٩٧ أَوَأَمِنَ أَهْلُ ٱلْقُرَىٰٓ أَن يَأْتِيَهُم بَأْسُنَا ضُحًۭى وَهُمْ يَلْعَبُونَ ٩٨ أَفَأَمِنُوا۟ مَكْرَ ٱللَّهِ ۚ فَلَا يَأْمَنُ مَكْرَ ٱللَّهِ إِلَّا ٱلْقَوْمُ ٱلْخَـٰسِرُونَ ٩٩ أَوَلَمْ يَهْدِ لِلَّذِينَ يَرِثُونَ ٱلْأَرْضَ مِنۢ بَعْدِ أَهْلِهَآ أَن لَّوْ نَشَآءُ أَصَبْنَـٰهُم بِذُنُوبِهِمْ ۚ وَنَطْبَعُ عَلَىٰ قُلُوبِهِمْ فَهُمْ لَا يَسْمَعُونَ ١٠٠ تِلْكَ ٱلْقُرَىٰ نَقُصُّ عَلَيْكَ مِنْ أَنۢبَآئِهَا ۚ وَلَقَدْ جَآءَتْهُمْ رُسُلُهُم بِٱلْبَيِّنَـٰتِ فَمَا كَانُوا۟ لِيُؤْمِنُوا۟ بِمَا كَذَّبُوا۟ مِن قَبْلُ ۚ كَذَٰلِكَ يَطْبَعُ ٱللَّهُ عَلَىٰ قُلُوبِ ٱلْكَـٰفِرِينَ ١٠١ وَمَا وَجَدْنَا لِأَكْثَرِهِم مِّنْ عَهْدٍۢ ۖ وَإِن وَجَدْنَآ أَكْثَرَهُمْ لَفَـٰسِقِينَ ١٠٢ ثُمَّ بَعَثْنَا مِنۢ بَعْدِهِم مُّوسَىٰ بِـَٔايَـٰتِنَآ إِلَىٰ فِرْعَوْنَ وَمَلَإِي۟هِۦ فَظَلَمُوا۟ بِهَا ۖ فَٱنظُرْ كَيْفَ كَانَ عَـٰقِبَةُ ٱلْمُفْسِدِينَ ١٠٣ وَقَالَ مُوسَىٰ يَـٰفِرْعَوْنُ إِنِّى رَسُولٌۭ مِّن رَّبِّ ٱلْعَـٰلَمِينَ ١٠٤"
+        ],
+      },
+      juz10: {
+        audio:
+          "https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/10.mp3",
+        ayyahs: [
+       "۞ وَٱعْلَمُوٓا۟ أَنَّمَا غَنِمْتُم مِّن شَىْءٍۢ فَأَنَّ لِلَّهِ خُمُسَهُۥ وَلِلرَّسُولِ وَلِذِى ٱلْقُرْبَىٰ وَٱلْيَتَـٰمَىٰ وَٱلْمَسَـٰكِينِ وَٱبْنِ ٱلسَّبِيلِ إِن كُنتُمْ ءَامَنتُم بِٱللَّهِ وَمَآ أَنزَلْنَا عَلَىٰ عَبْدِنَا يَوْمَ ٱلْفُرْقَانِ يَوْمَ ٱلْتَقَى ٱلْجَمْعَانِ ۗ وَٱللَّهُ عَلَىٰ كُلِّ شَىْءٍۢ قَدِيرٌ"
+        ],
+      },
+  };
+  
+  const juz = [
+    {
+      title: "Juz 1",
+      key: "juz1",
+      callback: getQuran,
+    },
+    {
+      title: "Juz 2",
+      key: "juz2",
+      callback: getQuran,
+    },
+    {
+      title: "Juz 3",
+      key: "juz3",
+      callback: getQuran,
+    },
+    {
+      title: "Juz 4",
+      key: "juz4",
+      callback: getQuran,
+    },
+    {
+        title: "Juz 5",
+        key: "juz5",
+        callback: getQuran,
+      },
+      {
+        title: "Juz 6",
+        key: "juz6",
+        callback: getQuran,
+      },
+      {
+        title: "Juz 7",
+        key: "juz7",
+        callback: getQuran,
+      },
+      {
+        title: "Juz 8",
+        key: "juz8",
+        callback: getQuran,
+      },
+      {
+        title: "Juz 9",
+        key: "juz9",
+        callback: getQuran,
+      },
+      {
+        title: "Juz 10",
+        key: "juz10",
+        callback: getQuran,
+      },
+  ];
+  function getQuran(juzTitle, juzName) {
+    const ayahs = ayah[juzName]["ayyahs"];
+    const audioPath = ayah[juzName]["audio"];
+    const cardHeader = document.getElementById("card-header");
+  
+    cardHeader.innerHTML = juzTitle;
+  
+    const cardContent = document.getElementById("card-content");
+    let content = "";
+    for (let index = 0; index < ayahs.length; index++) {
+      content += `<li>${ayahs[index]}</li>`;
+    }
+    cardContent.innerHTML = content;
+  
+    // add Audio Child
+    const audio = document.createElement("audio");
+    audio.controls = true;
+    audio.src = `${audioPath}`;
+    cardContent.appendChild(audio);
+  }
+  renderSideMenuBar();
+  function renderSideMenuBar() {
+    const sideMenuBar = document.getElementById("side-menu-bar");
+    for (let index = 0; index < juz.length; index++) {
+      const button = document.createElement("button");
+      button.innerText = juz[index].title;
+      button.addEventListener("click", () => {
+        juz[index].callback(juz[index].title, juz[index].key);
+      });
+      sideMenuBar.appendChild(button);
+    }
+  }
+
+  // DropDownBar
+
+  function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
